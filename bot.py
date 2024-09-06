17f00de8e9fa516cbfd6761fb3a1e0a5eeadc5e4
FallenChromium <fallenchromium@gmail.com>
2024-09-06 14:27:17 +0300

    Add support for link parsing

--- bot.py
+++ bot.py
@@ -1,15 +1,22 @@
 import logging
 import hashlib
+import re
 import sys
 import asyncio
 import db
 import yamusic
-import os
 import config
 
 
 from aiogram import Bot, Dispatcher
-from aiogram.types import FSInputFile, InlineQuery, Message, ChosenInlineResult, InlineQueryResultAudio, URLInputFile
+from aiogram.types import (
+    FSInputFile,
+    InlineQuery,
+    Message,
+    ChosenInlineResult,
+    InlineQueryResultAudio,
+    URLInputFile,
+)
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.types import InputMediaAudio
 from aiogram.filters import Command
@@ -32,30 +39,70 @@ async def upload_placeholder(message: Message):
     result = await message.reply_audio(FSInputFile("./tagmp3_crank-2.mp3"))
     await message.reply(result.audio.file_id)
 
+
 @dp.inline_query()
 async def inline_search_audio(inline_query: InlineQuery):
     items = []
     query = inline_query.query
+
     if not query:
-        await bot.answer_inline_query(inline_query.id, results=items, cache_time=5)
-        return
-    result = yamusic.search(query=query)
-    if result:
+        pass
+    elif query.startswith("https://"):
+        # single-result mode
+        yandex_match = re.match(
+            r"https://music.yandex.(ru|by|kz|com)/album/(\d+)/track/(\d+)", query
+        )
+        if yandex_match:
+            album_id, track_id = yandex_match.group(2), yandex_match.group(3)
+            full_id = f"{track_id}:{album_id}"
+            track = yamusic.get_track_data(full_id)
+            result_id = hashlib.md5(str(full_id).encode()).hexdigest()
+            result_ids[result_id] = full_id
+            items.append(
+                InlineQueryResultAudio(
+                    id=result_id,
+                    audio_url=track.download_link,
+                    title=track.title,
+                    performer=track.artists,
+                    reply_markup=InlineKeyboardMarkup(
+                        row_width=1,
+                        inline_keyboard=[
+                            [
+                                InlineKeyboardButton(
+                                    text="Загружаем...", callback_data=full_id
+                                )
+                            ]
+                        ],
+                    ),
+                )
+            )
+    else:
+        result = yamusic.search(query=query)
+
         for track in result:
-            markup = InlineKeyboardMarkup(row_width=1, inline_keyboard=[
-                [InlineKeyboardButton(text = "Загружаем...", callback_data=track['id'])]
-            ])
-            result_id = hashlib.md5(str(track['id']).encode()).hexdigest()
-            result_ids[result_id] = track['id']
+            loading_markup = InlineKeyboardMarkup(
+                row_width=1,
+                inline_keyboard=[
+                    [
+                        InlineKeyboardButton(
+                            text="Загружаем...", callback_data=track["id"]
+                        )
+                    ]
+                ],
+            )
+            result_id = hashlib.md5(str(track["id"]).encode()).hexdigest()
+            result_ids[result_id] = track["id"]
             item = InlineQueryResultAudio(
                 id=result_id,
                 audio_url="https://cs12.spac.me/f/022155028048118178004245225041194015065048227225162088184137/1651935767/90406904/0/912c9cca2ba0f5da4d0df7cb4f7483ea/placeholder-spcs.me.mp3#asdasd",
                 title=f"{track['caption']}",
                 performer=f"{track['artist']}",
-                reply_markup=markup
+                reply_markup=loading_markup,
             )
             items.append(item)
+
     await bot.answer_inline_query(inline_query.id, results=items, cache_time=5)
+
 
 @dp.chosen_inline_result()
 async def chosen_track(chosen_inline_result: ChosenInlineResult):
@@ -78,24 +125,30 @@ async def chosen_track(chosen_inline_result: ChosenInlineResult):
                 performer=str(data.artists),
                 thumbnail=URLInputFile(data.cover_url),
                 duration=data.duration,
-                chat_id=config.DUMP_CHAT_ID
+                chat_id=config.DUMP_CHAT_ID,
             )
 
             tg_file_id = file.audio.file_id
             db.save(track_id, tg_file_id)
 
         await bot.edit_message_media(
-                media=InputMediaAudio(media=tg_file_id, 
+            media=InputMediaAudio(
+                media=tg_file_id,
                 title=data.title,
-                performer=str(data.artists), 
-                thumbnail=URLInputFile(data.cover_url), 
+                performer=str(data.artists),
+                thumbnail=URLInputFile(data.cover_url),
                 duration=data.duration,
-                ),
-                inline_message_id=chosen_inline_result.inline_message_id
-            )
-        await bot.edit_message_caption(inline_message_id=chosen_inline_result.inline_message_id, caption=f"<a href='{data.link}'>Yandex Music</a>\n<a href='{odesli.getByUrl(data.link).songLink}'>song.link</a>", parse_mode='HTML')
+            ),
+            inline_message_id=chosen_inline_result.inline_message_id,
+        )
+        await bot.edit_message_caption(
+            inline_message_id=chosen_inline_result.inline_message_id,
+            caption=f"<a href='{data.link}'>Yandex Music</a>\n<a href='{odesli.getByUrl(data.link).songLink}'>song.link</a>",
+            parse_mode="HTML",
+        )
     else:
-        raise("Unknown result id")
+        raise ("Unknown result id")
+
 
 async def main() -> None:
     await dp.start_polling(bot)
@@ -104,4 +157,3 @@ async def main() -> None:
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
-
